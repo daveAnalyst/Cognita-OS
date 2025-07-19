f14d63443e47ffebb606e5406394c7a9c519edd8
dave <davidomungala3@gmail.com>
2025-07-19 12:04:39 -0700

    feat(frontend): successfully rebuild project infrastructure and implement v1 design system

--- src-frontend/postcss.config.mjs
+++ src-frontend/postcss.config.mjs
@@ -1,10 +1,5 @@
-// postcss.config.mjs
-/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // This is the key line that fixes the error
-    autoprefixer: {},
-  },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
